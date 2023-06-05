8532c4b1c1ea233a1e5be460f2965b758b1b35e7
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2023-06-05 14:35:07 +0300

    added info for 5 paintings and altered class folder naming

--- nn_model/dataset_generator.py
+++ nn_model/dataset_generator.py
@@ -11,7 +11,7 @@ load_dotenv()
 dataset_path = os.getenv('DATASET_PATH')
 init_image_path = os.getenv('INITIAL_IMAGE_PATH')
 
-dest_dir_name = 'validation'
+dest_dir_name = 'training'
 
 
 # names = ['zh-3269', 'zh-3273', 'zh-3299',
@@ -28,13 +28,13 @@ for i in range(class_amt):
     img_a = Image.open(os.getenv('INITIAL_IMAGE_PATH') + f"{i}.jpg")
 
     try:
-        os.mkdir(os.path.join(dataset_path, dest_dir_name, str(i)))
+        os.mkdir(os.path.join(dataset_path, dest_dir_name, f'{i//10}{i%10}'))
     except Exception:
         pass
 
     for x in range(n):
         img_b = get_image_transform(dest_dir_name)(img_a)
-        img_b.save(f'dataset/{dest_dir_name}/{i}/{x}.jpg')
+        img_b.save(f'dataset/{dest_dir_name}/{i//10}{i%10}/{x}.jpg')
         print(
             f'{i} ({i+1}/{class_amt}): {progress_bar(x+1, n)}',
             end='\r')
