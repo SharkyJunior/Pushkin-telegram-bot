7d06f5a42db64e03e898aada75bf0429406ebba5
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2023-06-13 02:19:13 +0300

    added 5 more paintings

--- nn_model/dataset_generator.py
+++ nn_model/dataset_generator.py
@@ -17,7 +17,7 @@ dest_dir_name = 'training'
 # names = ['zh-3269', 'zh-3273', 'zh-3299',
 #          'zh-3309', 'zh-3335', 'zh-3372', 'zh-3405']
 n = 300
-class_amt = 12
+class_amt = 17
 
 try:
     os.mkdir(os.path.join(dataset_path, dest_dir_name))
